860f09d5c251d147a5444c8c9ba6325b13d9071b
Utkarsh102002 <128974569+Utkarsh102002@users.noreply.github.com>
2025-04-11 11:33:56 +0000

    datatype

---
+++ test.js
@@ -0,0 +1,12 @@
+const accountId = 12345
+let accountEmail = "utkarshshrivastavagmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+
+// accountId = 2
+accountEmail = "yoyouttu1026@gmail.com"
+accountPassword ="12321"
+accountCity = "mumbai"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ test1.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log(3+3);
+
+let name = "uttu"
+let age = 18
+let isLoggedIn = false
+let state = null
+console.log(typeof state);
